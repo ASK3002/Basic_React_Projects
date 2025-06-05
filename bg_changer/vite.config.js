269ba1bb2e4ca88ff6f241e61074335ef8c295f8
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-05 20:25:13 +0530

    Fixed Vite base paths and homepage links

--- bg_changer/vite.config.js
+++ bg_changer/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Basic_React_Projects/bg_changer/', // 👈 this is critical
   plugins: [react()],
 })
--- counterUsingReact/vite.config.js
+++ counterUsingReact/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Counter-Using-React/',  // Add this line with your repo name, including slashes
+  base: '/Basic_React_Projects/counterUsingReact/',
   plugins: [react()],
 })
