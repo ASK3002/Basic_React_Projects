5e3cbf868e7439345a4858730d91b051ce697a77
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-05 20:44:06 +0530

    Set relative base path in vite.config.js for GitHub Pages

--- bg_changer/vite.config.js
+++ bg_changer/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Basic_React_Projects/bg_changer/', // 👈 this is critical
+  base: './', // 👈 this is critical
   plugins: [react()],
 })
