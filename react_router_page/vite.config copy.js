982f8061d6b28c88b2c5cd3942498943dd179f7c
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-10 22:26:43 +0530

    Remove unused assets and configuration files from the react_router_page project, including CSS, HTML, Vite configuration, and SVG files.

--- react_router_page/vite.config copy.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Basic_React_Projects/react_router_page/',
-  plugins: [react()],
-})
-
