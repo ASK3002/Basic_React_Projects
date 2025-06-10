1c0dac1bbb8efbaea437a2e5e162584e8598a1b6
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-10 21:09:57 +0530

    Add build for GitHub Pages with correct base

--- react_router_page/vite.config.js
+++ react_router_page/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  basename: '/react_router_page',  // <- IMPORTANT
+  basename: '/react_router_page/',  // <- IMPORTANT
   plugins: [react()],
 })
